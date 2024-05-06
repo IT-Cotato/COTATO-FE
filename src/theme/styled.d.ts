580d83e8c6adea551e301f6535dba84eabd0d5bc
Donghyun Hwang <eastring@elicer.com>
2024-05-07 01:14:54 +0900

    feat: apply new theme

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -4,13 +4,13 @@ import { LegacyThemeType, CotatoDarkThemeType, CotatoLightThemeType } from './th
 /**
  * Will be deprecated in v2.
  */
-declare module 'styled-components' {
-  export interface DefaultTheme extends LegacyThemeType {}
-}
+// declare module 'styled-components' {
+//   export interface DefaultTheme extends LegacyThemeType {}
+// }
 
 /**
  * Will be replaced this in v2.
  */
-// declare module 'styled-components' {
-//   export interface DefaultTheme extends CotatoLightThemeType, CotatoDarkThemeType {}
-// }
+declare module 'styled-components' {
+  export interface DefaultTheme extends CotatoLightThemeType, CotatoDarkThemeType {}
+}
