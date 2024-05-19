7c8887800554ce612874b295f253f54e207f4d75
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-05-19 18:35:28 +0900

    feat: implement footer

---
+++ src/components/NewFooter.tsx
@@ -0,0 +1,83 @@
+import React, { useCallback } from 'react';
+import { styled } from 'styled-components';
+import { ReactComponent as Insta } from '@/assets/footer_insta.svg';
+import { ReactComponent as Youtube } from '@/assets/footer_youtube.svg';
+import { ReactComponent as Github } from '@/assets/footer_github.svg';
+import { ReactComponent as Cafe } from '@/assets/footer_cafe.svg';
+
+const FooterWrapper = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 6.25rem 6.25rem 3.125rem 6.25rem;
+  gap: 6.25rem;
+`;
+
+const SnsWrapper = styled.div`
+  display: flex;
+  gap: 2.4rem;
+`;
+
+const DescriptionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.6rem;
+`;
+
+const MainText = styled.p`
+  font-family: Ycomputer;
+  font-size: ${({ theme }) => theme.fontSize.lg};
+`;
+
+const SubTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    font-family: Ycomputer;
+    font-size: ${({ theme }) => theme.fontSize.md};
+    line-height: 180%;
+  }
+`;
+
+const Footer = () => {
+  const renderSns = useCallback(
+    () => (
+      <SnsWrapper>
+        <Insta
+          onClick={() => open('https://www.instagram.com/cotato_official_?igsh=aDh2ZGZqazBpMmtp')}
+        />
+        <Youtube onClick={() => open('https://www.youtube.com/@ITCotato')} />
+        <Github onClick={() => open('https://github.com/IT-Cotato')} />
+        <Cafe onClick={() => open('https://cafe.naver.com/cotato')} />
+      </SnsWrapper>
+    ),
+    [],
+  );
+
+  const renderDescription = useCallback(
+    () => (
+      <DescriptionWrapper>
+        <MainText>Cotato 코테이토</MainText>
+        <SubTextContainer>
+          <p>E-mail. itcotato@gmail.com</p>
+          <p>© Cotato. 2024 All rights reserved. | 대학생 IT 연합 동아리 코테이토</p>
+          <p>Cotato 2024 HomePage v1.0</p>
+        </SubTextContainer>
+      </DescriptionWrapper>
+    ),
+    [],
+  );
+
+  return (
+    <FooterWrapper>
+      {renderSns()}
+      {renderDescription()}
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
