abcff6f61c92a3e85bcbdcd2d416423ac08a146a
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-06-24 02:20:49 +0900

    add: connect api and redirect if already logged in

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as WelcomeImg } from '@assets/login_welcome_img.svg';
 import idIcon from '@assets/login_id_icon.svg';
@@ -8,6 +8,9 @@ import btnHover from '@assets/login_btn_hover.svg';
 import btnClicked from '@assets/login_btn_clicked.svg';
 import line from '@assets/login_line.svg';
 import { Link } from 'react-router-dom';
+import api from '@/api/api';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
 
 //
 //
@@ -15,18 +18,73 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [btnState, setBtnState] = useState('default');
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [isError, setIsError] = useState(false);
+
+  const { data, mutate } = useSWR('/v1/api/member/info', fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+    // 서버 리소스를 한번 받아오고 나서는 다시 받아오지 않음
+  });
+
+  const handleChangeId = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(e.target.value);
+  }, []);
+
+  const handleChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setIsError(false);
+      api
+        .post('/login', {
+          email: id,
+          password: password,
+        })
+        .then((res) => {
+          localStorage.setItem('token', res.headers.accesstoken);
+          mutate('/v1/api/member/info'); // 로그인 후에는 swr 요청을 수동으로 해준다
+        })
+        .catch((error) => {
+          console.log(error);
+          setIsError(true);
+          if (id === '' || password === '') {
+            alert('아이디 또는 비밀번호를 입력해주세요');
+          } else {
+            alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+          }
+        });
+    },
+    [id, password],
+  );
+
+  if (data) {
+    console.log(data);
+    window.location.href = '/';
+  }
 
   return (
     <Wrapper>
       <WelcomeImg style={{ position: 'absolute', zIndex: 10, width: '360px' }} />
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <InputBox>
           <img src={idIcon} />
-          <input type="text" id="id" name="id" placeholder="아이디" />
+          <input type="text" id="id" name="id" placeholder="아이디" onChange={handleChangeId} />
         </InputBox>
         <InputBox>
           <img src={passwordIcon} />
-          <input type="text" id="id" name="id" placeholder="비밀번호" />
+          <input
+            type="text"
+            id="id"
+            name="id"
+            placeholder="비밀번호"
+            onChange={handleChangePassword}
+          />
         </InputBox>
         <div style={{ height: '84px', display: 'flex', alignItems: 'end', marginTop: '12px' }}>
           <LoginBtn
