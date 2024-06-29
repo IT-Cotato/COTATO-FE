25044d6f0ec2e099d8ddb4ffe2eb57fc0b8bda27
Donghyun Hwang <eastring@elicer.com>
2024-06-29 23:28:08 +0900

    chore: add space

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ import GlobalBackgroundSvgComponent from '@components/GlobalBackgroundSvgCompone
 function App() {
   const location = useLocation();
   const isInHome = location.pathname === '/';
+
   //
   //
   //
