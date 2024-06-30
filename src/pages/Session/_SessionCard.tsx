85b96489e4b0c5b062507d1532493512d1044bf4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-06-30 13:10:19 +0900

    feat: implement session card

---
+++ src/pages/Session/_SessionCard.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import styled, { useTheme } from 'styled-components';
+import { ReactComponent as HeartIcon } from '@assets/heart_icon_dotted.svg';
+import ready_image from '@assets/ready_potato.svg';
+
+interface SessionInfo {
+  sessionId: number;
+  sessionNumber: number;
+  photoUrl: string;
+  description: string;
+  generationId: number;
+  sessionContents: {
+    itIssue: 'IT_ON' | 'IT_OFF';
+    networking: 'NW_ON' | 'NW_OFF';
+    csEducation: 'CS_ON' | 'CS_OFF';
+    devTalk: 'DEVTALK_ON' | 'DEVTALK_OFF';
+  };
+}
+
+interface SessionCardProps {
+  session: SessionInfo;
+}
+
+const SessionCard = ({ session }: SessionCardProps) => {
+  const theme = useTheme();
+
+  const renderCardHeader = () => (
+    <CardHeader>
+      <Circle>
+        <HeartIcon />
+      </Circle>
+      {session.sessionNumber}주차 세션
+    </CardHeader>
+  );
+
+  const renderCardImage = () => <ReadyImage src={ready_image} />;
+
+  const renderSessionContents = () => {
+    const { csEducation, itIssue, networking, devTalk } = session.sessionContents;
+
+    return (
+      <SessionContentsWrapper>
+        {csEducation === 'CS_ON' && <Content $color={theme.colors.primary100_1}>#CS</Content>}
+        {itIssue === 'IT_ON' && <Content $color={theme.colors.sub2[80]}>#IT</Content>}
+      </SessionContentsWrapper>
+    );
+  };
+
+  return (
+    <Container>
+      {renderCardHeader()}
+      {renderCardImage()}
+      {renderSessionContents()}
+    </Container>
+  );
+};
+
+//
+//
+//
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20rem;
+  border: 3px solid ${({ theme }) => theme.colors.primary100_1};
+  background: ${({ theme }) => theme.colors.common.white};
+`;
+
+const CardHeader = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: ${({ theme }) => theme.size.lg};
+  padding: ${({ theme }) => theme.size.md} ${({ theme }) => theme.size.lg};
+  font-family: Ycomputer;
+  font-size: ${({ theme }) => theme.fontSize.xl};
+`;
+
+const Circle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.colors.primary100_1};
+`;
+
+const ReadyImage = styled.img`
+  width: 100%;
+`;
+
+const SessionContentsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: ${({ theme }) => theme.size.sm};
+  padding: ${({ theme }) => theme.size.lg};
+`;
+
+const Content = styled.div<{ $color: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: ${({ theme }) => theme.size.sm} ${({ theme }) => theme.size.xl};
+  border-radius: 6.2rem;
+  background: ${({ $color }) => $color};
+  font-family: Ycomputer;
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  color: ${({ theme }) => theme.colors.common.white};
+`;
+
+export default SessionCard;
