153e0cd8cb6547333b916284948cf648e9b0f88e
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-07-04 00:29:25 +0900

    feat: make enum for session contests

---
+++ src/enums/SessionContents.ts
@@ -0,0 +1,43 @@
+/**
+ * enum for session contents that cs education took place
+ *
+ * @description **ON** session that was CS education took place
+ * @description **OFF** session that was CS education did not take place
+ */
+export enum SessionContentsCsEducation {
+  ON = 'CS_ON',
+  OFF = 'CS_OFF',
+}
+
+/**
+ * enum for session contents that IT issue took place
+ *
+ * @description **ON** session that was IT issue took place
+ * @description **OFF** session that was IT issue did not take place
+ */
+export enum SessionContentsItIssue {
+  ON = 'IT_ON',
+  OFF = 'IT_OFF',
+}
+
+/**
+ * enum for session contents that networking took place
+ *
+ * @description **ON** session that was networking took place
+ * @description **OFF** session that was networking did not take place
+ */
+export enum SessionContentsNetworking {
+  ON = 'NW_ON',
+  OFF = 'NW_OFF',
+}
+
+/**
+ * enum for session contents that dev talk took place
+ *
+ * @description **ON** session that was dev talk took place
+ * @description **OFF** session that was dev talk did not take place
+ */
+export enum SessionContentsDevTalk {
+  ON = 'DEVTALK_ON',
+  OFF = 'DEVTALK_OFF',
+}
