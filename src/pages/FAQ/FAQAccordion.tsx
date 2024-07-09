373fa04af2e0136943941fe4798cf1ca561781ca
Donghyun Hwang <eastring@elicer.com>
2024-07-09 23:16:06 +0900

    feat: implement faq

---
+++ src/pages/FAQ/FAQAccordion.tsx
@@ -0,0 +1,204 @@
+import React from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Stack,
+  Typography,
+  accordionDetailsClasses,
+  accordionSummaryClasses,
+  accordionClasses,
+  useMediaQuery,
+} from '@mui/material';
+import styled from 'styled-components';
+import { ReactComponent as CharacterEyeClose } from '@assets/character_eye_close.svg';
+import { device, media } from '@theme/media';
+
+//
+//
+//
+
+interface FAQAccordionProps {
+  question: string;
+  answer: string;
+  summary: string;
+  hash: string;
+  expanded: boolean;
+  index?: number;
+  onClick?: (hash: string) => void;
+}
+
+//
+//
+//
+
+const FAQAccordion: React.FC<FAQAccordionProps> = ({
+  question,
+  answer,
+  hash,
+  expanded,
+  index = 0,
+  onClick,
+}) => {
+  const isMobileOrSmaller = useMediaQuery(`(max-width:${device.mobile})`);
+  //
+  //
+  //
+
+  /**
+   *
+   */
+  const handleAccordionClick = () => {
+    if (typeof onClick !== 'function') return;
+
+    onClick(hash);
+  };
+
+  /**
+   *
+   */
+  const renderAccordionSummary = () => {
+    return (
+      <StyledAccordionSummary
+        expandIcon={<span className="material-symbols-outlined">chevron_right</span>}
+        onClick={handleAccordionClick}
+        sx={{
+          padding: { xs: '1rem 2rem', md: '1.5rem 2rem' },
+        }}
+      >
+        <QustionIndex>Q{index + 1}</QustionIndex>
+        <Typography
+          fontFamily="Pretendard"
+          fontWeight="bold"
+          sx={{
+            fontSize: { xs: '0.875rem', md: '1rem' },
+          }}
+        >
+          {question}
+        </Typography>
+      </StyledAccordionSummary>
+    );
+  };
+
+  /**
+   *
+   */
+  const renderCharacter = () => {
+    if (isMobileOrSmaller) {
+      return null;
+    }
+
+    return (
+      <Stack width="6rem" padding="1rem" justifyContent="center" alignItems="flex-end">
+        <CharacterEyeClose width="3rem" height="3.5rem" />
+      </Stack>
+    );
+  };
+
+  /**
+   *
+   */
+  const renderAccordionDetails = () => {
+    return (
+      <StyledAccordionDetails>
+        <AnswerIndex>A{index + 1}</AnswerIndex>
+        <Stack width="25rem" justifyContent="center">
+          <StyledTypography
+            dangerouslySetInnerHTML={{ __html: answer }}
+            lineHeight="1.75rem"
+            fontFamily="Pretendard"
+            fontSize={{ xs: '0.875rem', md: '1rem' }}
+            sx={{
+              wordBreak: 'keep-all',
+            }}
+          />
+        </Stack>
+        {renderCharacter()}
+      </StyledAccordionDetails>
+    );
+  };
+
+  //
+  //
+  //
+
+  return (
+    <StyledAccordion defaultExpanded={expanded}>
+      {renderAccordionSummary()}
+      {renderAccordionDetails()}
+    </StyledAccordion>
+  );
+};
+
+export default FAQAccordion;
+
+const StyledAccordion = styled(Accordion)(() => ({
+  [`&.${accordionClasses.root}`]: {
+    background: 'none',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '1rem',
+    boxShadow: 'none',
+  },
+}));
+
+const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+  [`&.${accordionSummaryClasses.root}`]: {
+    borderRadius: '0.5rem',
+    border: `2px solid ${theme.colors.primary100_2}`,
+    backgroundColor: theme.colors.common.white,
+    color: theme.colors.common.black,
+  },
+
+  [`& .${accordionSummaryClasses.expandIconWrapper}`]: {
+    color: theme.colors.secondary60,
+    '&.Mui-expanded': {
+      transform: 'rotate(90deg)',
+    },
+  },
+}));
+
+const StyledAccordionDetails = styled(AccordionDetails)(({ theme }) => ({
+  [`&.${accordionDetailsClasses.root}`]: {
+    display: 'flex',
+    padding: '1rem 2rem',
+    border: `2px solid ${theme.colors.sub2['20']}`,
+    borderRadius: '0.5rem',
+    color: theme.colors.common.black,
+    backgroundColor: theme.colors.common.white,
+  },
+}));
+
+const QustionIndex = styled.div`
+  position: absolute;
+  left: 0.5rem;
+  top: -0.5rem;
+  width: 2rem;
+  height: 1rem;
+  text-align: center;
+  background-color: ${({ theme }) => theme.colors.common.white};
+  color: ${({ theme }) => theme.colors.primary100_2};
+`;
+
+const AnswerIndex = styled.div`
+  position: relative;
+  left: -1.25rem;
+  top: -1.5rem;
+  width: 2rem;
+  height: 1rem;
+  text-align: center;
+  background-color: ${({ theme }) => theme.colors.common.white};
+  color: ${({ theme }) => theme.colors.sub2['20']};
+`;
+
+const StyledTypography = styled(Typography)`
+  * {
+    font-family: Pretendard;
+    & + li {
+      margin-top: 0.5rem;
+    }
+  }
+  ul {
+    padding-left: 1rem;
+  }
+`;
---
+++ src/pages/FAQ/FAQFloatingNav.tsx
@@ -0,0 +1,71 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+import styled from 'styled-components';
+
+//
+//
+//
+
+interface FAQFloatingNavProps {
+  title: string;
+  summaries: string[];
+  hashes: string[];
+  handleNavClick: (hash: string) => void;
+}
+
+//
+//
+//
+
+const FAQFloatingNav: React.FC<FAQFloatingNavProps> = ({
+  title,
+  summaries,
+  hashes,
+  handleNavClick,
+}) => {
+  //
+  //
+  //
+
+  return (
+    <Wrapper>
+      <StyledTitle>{title}</StyledTitle>
+      <Stack gap="0.5rem">
+        {hashes.map((hash, idx) => (
+          <StyledStack
+            key={idx}
+            gap="0.25rem"
+            padding="0.5rem 1rem"
+            direction="row"
+            borderRadius="0.25rem"
+            justifyContent="flex-start"
+            onClick={() => handleNavClick(hash)}
+          >
+            &#x2022;
+            {summaries[idx]}
+          </StyledStack>
+        ))}
+      </Stack>
+    </Wrapper>
+  );
+};
+
+export default FAQFloatingNav;
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.common.white};
+`;
+
+const StyledTitle = styled.div`
+  color: ${({ theme }) => theme.colors.primary100_2};
+  font-family: 'Pretendard';
+  font-weight: bold;
+  font-size: ${({ theme }) => theme.fontSize.xxl};
+  margin-bottom: 1rem;
+`;
+
+const StyledStack = styled(Stack)`
+  background-color: ${({ theme }) => theme.colors.gray10};
+  color: ${({ theme }) => theme.colors.gray100};
+  cursor: pointer;
+`;
