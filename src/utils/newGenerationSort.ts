228b47fd6162859592adf6e55d6ff29d7049d5bf
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-07-10 00:44:42 +0900

    add: declare return type

--- src/utils/newGenerationSort.ts
+++ src/utils/newGenerationSort.ts
@@ -1,6 +1,8 @@
 import { CotatoGenerationInfoResponse } from 'cotato-openapi-clients';
 
-const generationSort = (generations: CotatoGenerationInfoResponse[]) => {
+const generationSort = (
+  generations: CotatoGenerationInfoResponse[],
+): CotatoGenerationInfoResponse[] => {
   return generations.sort((left, right) => {
     if (left?.generationNumber && right?.generationNumber) {
       return right.generationNumber - left.generationNumber;
