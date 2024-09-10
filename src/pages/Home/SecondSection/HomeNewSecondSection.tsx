1394acc1a83c880c75dfd879004ded3667d4433f
Donghyun Hwang <eastring@elicer.com>
2024-09-11 00:27:31 +0900

    feat: implement new section two

--- src/pages/Home/SecondSection/HomeNewSecondSection.tsx
+++ src/pages/Home/SecondSection/HomeNewSecondSection.tsx
@@ -1,20 +1,66 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import HomeSecondSectionCard from './HomeSecondSectionCard';
-import cs from '@assets/CS.svg';
+
+import itLight from '@assets/it_light.svg';
+import itDark from '@assets/it_dark.svg';
+import csLight from '@assets/cs_light.svg';
+import csDark from '@assets/cs_dark.svg';
+import networkLight from '@assets/network_light.svg';
+import networkDark from '@assets/network_dark.svg';
+import hackathonLight from '@assets/hackathon_light.svg';
+import hackathonDark from '@assets/hackathon_dark.svg';
+import devtalkLight from '@assets/devtalk_light.svg';
+import devtalkDark from '@assets/devtalk_dark.svg';
+import demodayLight from '@assets/demoday_light.svg';
+import demodayDark from '@assets/demoday_dark.svg';
+
+import { ReactComponent as ItIcon } from '@assets/it_icon.svg';
 import { ReactComponent as CsIcon } from '@assets/cs_icon.svg';
+import { ReactComponent as NetworkIcon } from '@assets/network_icon.svg';
+import { ReactComponent as HackathonIcon } from '@assets/hackathon_icon.svg';
+import { ReactComponent as DevtalkIcon } from '@assets/devtalk_icon.svg';
+import { ReactComponent as DemodayIcon } from '@assets/demoday_icon.svg';
 import { ReactComponent as SectionTwoTitle } from '@assets/home_section2_title.svg';
 import styled, { useTheme } from 'styled-components';
 import { media } from '@theme/media';
 import styles from './style.module.css';
 import { CotatoThemeType } from '@theme/theme';
-import { Autoplay, FreeMode } from 'swiper/modules';
+import { FreeMode } from 'swiper/modules';
+import { ThemeContext } from '@theme/context/CotatoThemeProvider';
 
 //
 //
 //
 
-const SWIPER_SPEED = 1200;
+const LIGHT_ASSETS = [itLight, csLight, networkLight, hackathonLight, devtalkLight, demodayLight];
+const DARK_ASSETS = [itDark, csDark, networkDark, hackathonDark, devtalkDark, demodayDark];
+const CARD_INFOS = [
+  { title: 'IT 이슈', description: '각 팀별로 최신 IT 이슈에 대해 조사하고 발표합니다.' },
+  {
+    title: 'CS 교육',
+    description: '개발자, 기획자, 디자이너에게 도움이 될 다양한 주제의 CS 지식을 배웁니다.',
+  },
+  {
+    title: '네트워킹',
+    description: '각 파트별로 네트워킹 시간을 가지며 관련 정보와 지식을 공유합니다.',
+  },
+  { title: '해커톤', description: '무박 2일 간 단기 프로젝트를 통해 IT 서비스를 제작합니다. ' },
+  { title: '데브토크', description: '코테이토 출신 현업자 분을 초청해 강연을 진행합니다.' },
+  {
+    title: '데모데이',
+    description: '주제별로 6개월 간 진행한 스터디와 프로젝트에서 각자 완성한 결과물을 발표합니다.',
+  },
+];
+const ICON_ASSETS = [
+  <ItIcon />,
+  <CsIcon />,
+  <NetworkIcon />,
+  <HackathonIcon />,
+  <DevtalkIcon />,
+  <DemodayIcon />,
+];
 
 //
 //
@@ -22,39 +68,26 @@ const SWIPER_SPEED = 1200;
 
 const HomeNewSecondSection = () => {
   const theme = useTheme();
+  const { DefaultTheme } = React.useContext(ThemeContext);
+
   //
   //
   //
+
   return (
     <Wrapper>
       <Title>
         <SectionTwoTitle fill={theme.colors.gray80_2} className={styles['section_two_title']} />
         <p>코테이토의 활동을 소개할게요!</p>
       </Title>
-      <StyledSwiper
-        autoplay={{
-          delay: 3000,
-        }}
-        freeMode={true}
-        modules={[Autoplay, FreeMode]}
-        speed={SWIPER_SPEED}
-        slidesPerView="auto"
-        spaceBetween={100}
-      >
+      <StyledSwiper freeMode={true} slidesPerView="auto" spaceBetween={25} modules={[FreeMode]}>
         {Array.from({ length: 6 }).map((_, index) => (
-          <StyledSwiperSlide
-            key={index}
-            style={{
-              marginTop: index % 2 === 0 ? '4rem !important' : '0rem',
-              marginBottom: index % 2 !== 0 ? '4rem !important' : '0rem',
-            }}
-            margin={index % 2 === 0 ? '4rem 0 0 0' : '0 0 4rem 0'}
-          >
+          <StyledSwiperSlide key={index}>
             <HomeSecondSectionCard
-              imgSrc={cs}
-              title="IT 이슈"
-              description="각 팀별로 최신 IT 이슈에 대해 조사하고 발표합니다."
-              icon={<CsIcon />}
+              imgSrc={DefaultTheme === 'light' ? LIGHT_ASSETS[index] : DARK_ASSETS[index]}
+              title={CARD_INFOS[index].title}
+              description={CARD_INFOS[index].description}
+              icon={ICON_ASSETS[index]}
             />
           </StyledSwiperSlide>
         ))}
@@ -105,14 +138,25 @@ const StyledSwiper = styled(Swiper)`
   height: 100vh;
   padding-left: 6rem;
   padding-right: 6rem;
+  padding-top: 0.5rem;
+
+  ${media.tablet`
+    padding-left: 4rem;
+    padding-right: 4rem;
+  `}
+
+  ${media.mobile`
+    padding-left: 2.5rem;
+    padding-right: 2.5rem;
+  `}
 `;
 
-const StyledSwiperSlide = styled(SwiperSlide)<{ margin: string }>`
+const StyledSwiperSlide = styled(SwiperSlide)`
   width: 21rem;
-  &:nth-child(odd) {
+  &:nth-child(even) {
     margin-top: 4rem;
   }
-  &:nth-child(even) {
+  &:nth-child(odd) {
     margin-bottom: 4rem;
   }
 `;
--- src/pages/Home/SecondSection/HomeSecondSectionCard.tsx
+++ src/pages/Home/SecondSection/HomeSecondSectionCard.tsx
@@ -1,6 +1,7 @@
 import { Box, Card, CardContent, Stack, Typography } from '@mui/material';
+import { media } from '@theme/media';
 import React from 'react';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 
 //
 //
@@ -23,18 +24,57 @@ const HomeSecondSectionCard = ({
   title,
   description,
 }: HomeSecondSectionCardProps) => {
+  const theme = useTheme();
+
+  /**
+   * ref: https://www.youtube.com/watch?v=YDCCauu4lIk&t=341s
+   */
+  const onMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    const card = event.currentTarget;
+
+    const rect = card.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const rotateY = (-1 / 50) * x + 2;
+    const rotateX = (1 / 100) * y - 2;
+
+    card.style.transition = 'transform 0.1s';
+    card.style.transform = `perspective(350px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  };
+
+  /***
+   *
+   */
+  const onMouseLeave = (event: React.MouseEvent<HTMLDivElement>) => {
+    const card = event.currentTarget;
+    card.style.transition = 'transform 0.5s ease';
+    card.style.transform = 'perspective(400px) rotateX(0deg) rotateY(0deg)';
+  };
+
+  //
+  //
+  //
+
   return (
-    <StyledCard imgSrc={imgSrc}>
+    <StyledCard
+      onMouseMove={onMouseMove}
+      onMouseLeave={onMouseLeave}
+      sx={{
+        backgroundImage: `url(${imgSrc})`,
+        backgroundColor: 'transparent',
+      }}
+    >
       <StyledCardContent>
-        <Stack gap="1rem">
+        <Stack gap="1rem" padding="0rem 1.5rem 0.5rem 1.5rem">
           <Stack gap="0.5rem" direction="row">
             {icon}
-            <Typography variant="h4" fontWeight="700">
+            <Typography variant="h4" fontWeight="700" color={theme.colors.common.black}>
               {title}
             </Typography>
           </Stack>
-          <Box width="12rem">
-            <Typography variant="body1" fontFamily="YComputer">
+          <Box width="100%">
+            <Typography color={theme.colors.gray80_1} fontSize="0.875rem" lineHeight="1.2rem">
               {description}
             </Typography>
           </Box>
@@ -48,11 +88,21 @@ const HomeSecondSectionCard = ({
 //
 //
 
-const StyledCard = styled(Card)<{ imgSrc: string }>`
-  background-image: url(${(props) => props.imgSrc});
+const StyledCard = styled(Card)`
   background-size: cover;
   width: 21rem;
   height: 28rem;
+  border-radius: 0.625rem !important;
+
+  ${media.tablet`
+    width: 18rem;
+    height: 24rem;
+  `}
+
+  ${media.mobile`
+    width: 16rem;
+    height: 22rem;
+  `}
 `;
 
 const StyledCardContent = styled(CardContent)`
