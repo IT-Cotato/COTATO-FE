f092346076f7777b31b8e4fadc7b71977f68c4b4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-09-20 16:11:13 +0900

    feat: implement time picker

---
+++ src/components/CotatoTimePicker.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import dayjs, { Dayjs } from 'dayjs';
+import { useTheme } from 'styled-components';
+
+//
+//
+//
+
+interface CotatoTimePickerProps {
+  date: Date;
+  onDateChange: (date: Date) => void;
+}
+
+//
+//
+//
+
+const CotatoTimePicker: React.FC<CotatoTimePickerProps> = ({ date, onDateChange }) => {
+  const theme = useTheme();
+
+  const handleDateChange = (newDate: Dayjs | null) => {
+    onDateChange(newDate?.toDate() || date);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
+      <TimePicker
+        value={dayjs(date)}
+        onChange={(newDate) => handleDateChange(newDate)}
+        sx={{
+          ['& .MuiFormControl-root']: { width: '10rem' },
+          ['& .MuiInputBase-root']: {
+            border: `1px solid ${theme.colors.gray60}`,
+            borderRadius: '0.5rem',
+            background: theme.colors.common.white,
+            width: '11rem',
+          },
+          ['& .MuiInputBase-input']: {
+            fontFamily: 'Pretendard',
+            color: theme.colors.gray90,
+            fontSize: '0.875rem',
+            fontWeight: 300,
+            padding: '0.75rem',
+          },
+          ['& .MuiOutlinedInput-notchedOutline']: {
+            border: 'none',
+          },
+        }}
+      />
+    </LocalizationProvider>
+  );
+};
+
+//
+//
+//
+
+export default CotatoTimePicker;
