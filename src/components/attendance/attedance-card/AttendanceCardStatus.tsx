8d0d5532e36d9e404326a9b1441f0655a9e0c817
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-10-27 11:33:39 +0900

    feat: declare attendance status

--- src/components/attendance/attedance-card/AttendanceCardStatus.tsx
+++ src/components/attendance/attedance-card/AttendanceCardStatus.tsx
@@ -18,6 +18,16 @@ interface AttendaceStatusProps {
   attendanceResult?: AttendResponseAttendanceResultEnum | null;
 }
 
+enum AttendanceStatus {
+  Before,
+  Open,
+  PresentOffline,
+  PresentOnline,
+  LateOffline,
+  LateOnline,
+  Absent,
+}
+
 //
 //
 //
@@ -29,6 +39,96 @@ const AttendaceCardStatus: React.FC<AttendaceStatusProps> = ({
 }) => {
   const theme = useTheme();
 
+  /**
+   *
+   */
+  const getAttendanceStatus = () => {
+    if (isOpened === AttendResponseIsOpenedEnum.Before) {
+      return AttendanceStatus.Before;
+    } else if (isOpened === AttendResponseIsOpenedEnum.Open && attendanceResult === null) {
+      return AttendanceStatus.Open;
+    } else if (
+      attendanceResult === AttendResponseAttendanceResultEnum.Present &&
+      attendanceType === AttendResponseAttendanceTypeEnum.Offline
+    ) {
+      return AttendanceStatus.PresentOffline;
+    } else if (
+      attendanceResult === AttendResponseAttendanceResultEnum.Present &&
+      attendanceType === AttendResponseAttendanceTypeEnum.Online
+    ) {
+      return AttendanceStatus.PresentOnline;
+    } else if (
+      attendanceResult === AttendResponseAttendanceResultEnum.Late &&
+      attendanceType === AttendResponseAttendanceTypeEnum.Offline
+    ) {
+      return AttendanceStatus.LateOffline;
+    } else if (
+      attendanceResult === AttendResponseAttendanceResultEnum.Late &&
+      attendanceType === AttendResponseAttendanceTypeEnum.Online
+    ) {
+      return AttendanceStatus.LateOnline;
+    }
+
+    return AttendanceStatus.Absent;
+  };
+
+  const getStatusStyle = () => {
+    switch (getAttendanceStatus()) {
+      case AttendanceStatus.Before:
+        return {
+          backgroundColor: theme.colors.gray40,
+          icon: null,
+          textColor: theme.colors.common.white_const,
+          text: '출석예정',
+        };
+      case AttendanceStatus.Open:
+        return {
+          backgroundColor: theme.colors.primary80,
+          icon: null,
+          textColor: theme.colors.common.white_const,
+          text: '출석중',
+        };
+      case AttendanceStatus.PresentOffline:
+        return {
+          backgroundColor: theme.colors.gray10,
+          icon: <CotatoIcon icon="user-check-solid" color={(theme) => theme.colors.sub3[40]} />,
+          textColor: theme.colors.common.black_const,
+          text: '출석',
+        };
+      case AttendanceStatus.PresentOnline:
+        return {
+          backgroundColor: theme.colors.gray10,
+          icon: <OnlineIcon />,
+          textColor: theme.colors.common.black_const,
+          text: '출석',
+        };
+      case AttendanceStatus.LateOffline:
+        return {
+          backgroundColor: theme.colors.gray10,
+          icon: <CotatoIcon icon="user-check-solid" color={(theme) => theme.colors.sub3[40]} />,
+          textColor: theme.colors.common.black_const,
+          text: '출석',
+        };
+      case AttendanceStatus.LateOnline:
+        return {
+          backgroundColor: theme.colors.gray10,
+          icon: <OnlineIcon />,
+          textColor: theme.colors.common.black_const,
+          text: '출석',
+        };
+      case AttendanceStatus.Absent:
+        return {
+          backgroundColor: theme.colors.gray10,
+          icon: <AbsentIcon />,
+          textColor: theme.colors.common.black_const,
+          text: '결석',
+        };
+    }
+  };
+
+  /**
+   *
+   */
   const getSpeachBubbleColor = () => {
     if (isOpened === AttendResponseIsOpenedEnum.Before) {
       return theme.colors.gray40;
