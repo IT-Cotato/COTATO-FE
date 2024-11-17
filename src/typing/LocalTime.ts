81025afd8e147a50a371a031ea6fc4113466e81b
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-11-18 01:38:16 +0900

    feat: Allow undefined in LocalTime class constructor

--- src/typing/LocalTime.ts
+++ src/typing/LocalTime.ts
@@ -10,14 +10,37 @@ export default class LocalTime implements CotatoLocalTime {
   nano: number;
 
   /**
-   * @param {string} timeString
+   * @param {string} [timeString] - "HH:MM:SS" 형식의 시간 문자열 (옵션)
    */
-  constructor(timeString: string) {
-    const [hour, minute, second] = timeString.split(':').map(Number);
+  constructor(timeString?: string) {
+    if (timeString) {
+      const [hour, minute, second] = timeString.split(':').map(Number);
 
-    this.hour = hour;
-    this.minute = minute;
-    this.second = second;
+      if (
+        isNaN(hour) ||
+        isNaN(minute) ||
+        isNaN(second) ||
+        hour < 0 ||
+        hour > 23 ||
+        minute < 0 ||
+        minute > 59 ||
+        second < 0 ||
+        second > 59
+      ) {
+        throw new Error("Invalid time format. Expected 'HH:MM:SS'");
+      }
+
+      this.hour = hour;
+      this.minute = minute;
+      this.second = second;
+    } else {
+      const now = new Date();
+
+      this.hour = now.getHours();
+      this.minute = now.getMinutes();
+      this.second = now.getSeconds();
+    }
+
     this.nano = 0;
   }
 
