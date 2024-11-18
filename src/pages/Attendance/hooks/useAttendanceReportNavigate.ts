16e83407664d7853eabdbf08bc8e693eae9bab6d
WONYOUNG-HC <whdnjsdud551@gmail.com>
2024-11-19 03:34:03 +0900

    chore: write useAttendaneceReportNavigate hook description

--- src/pages/Attendance/hooks/useAttendanceReportNavigate.ts
+++ src/pages/Attendance/hooks/useAttendanceReportNavigate.ts
@@ -4,9 +4,24 @@ import { useNavigate } from 'react-router-dom';
 //
 //
 
+/**
+ * Custom hook to navigate to the attendance report page.
+ *
+ * The resulting URL format is:
+ * `/attendance/report/generation/{generationId}/session/{sessionId}/attendance/{attendanceId}`
+ *
+ * @returns {Function} A function that navigates to the specified attendance report page.
+ */
 const useAttendanceReportNavigate = () => {
   const navigate = useNavigate();
 
+  /**
+   * Navigate to the attendance report page for a specific generation, session, and attendance.
+   *
+   * @param generationId {number | string}
+   * @param sessionId {number | string}
+   * @param attendanceId {number | string}
+   */
   const attendanceReportNavigate = (
     generationId: number | string,
     sessionId: number | string,
