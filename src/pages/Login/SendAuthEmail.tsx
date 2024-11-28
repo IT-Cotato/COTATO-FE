fc47f2d93b0f43321ee84227bc34c39cf72a74ab
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-11-29 01:34:11 +0900

    feat: redesignn sending email page

---
+++ src/pages/Login/SendAuthEmail.tsx
@@ -0,0 +1,175 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CotatoPanel from '@components/CotatoPanel';
+import { Box } from '@mui/material';
+import CotatoButton from '@components/CotatoButton';
+import CotatoIcon from '@components/CotatoIcon';
+import panelText from '@assets/find_password_sending_email_panel_text.svg';
+import api from '@/api/api';
+
+//
+//
+//
+
+interface SendAuthEmailProps {
+  goToNextStep: () => void;
+  isEmail: boolean;
+  setIsEmail: React.Dispatch<React.SetStateAction<boolean>>;
+  isPassword: boolean;
+  isPasswordCheck: boolean;
+  email: string;
+  setEmail: React.Dispatch<React.SetStateAction<string>>;
+}
+
+//
+//
+//
+
+const EMAIL_REGEX =
+  /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+//
+//
+//
+
+const SendAuthEmail: React.FC<SendAuthEmailProps> = ({
+  goToNextStep,
+  isEmail,
+  setIsEmail,
+  isPassword,
+  isPasswordCheck,
+  email,
+  setEmail,
+}) => {
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const emailData = {
+    email: email,
+  };
+
+  /**
+   *
+   */
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const input = e.target.value;
+    setEmail(input);
+
+    if (!EMAIL_REGEX.test(input)) {
+      setErrorMsg('올바른 이메일 형식이 아닙니다.');
+      setIsEmail(false);
+    } else {
+      setErrorMsg('');
+      setIsEmail(true);
+    }
+  };
+
+  /**
+   *
+   */
+  const renderInputError = () => {
+    if (isEmail) {
+      return;
+    }
+
+    return (
+      <Error>
+        <CotatoIcon
+          icon="times-circle"
+          size="1.25rem"
+          color={(theme) => theme.colors.secondary80}
+        />
+        <span>{errorMsg}</span>
+      </Error>
+    );
+  };
+
+  /**
+   *
+   */
+  const renderInputField = () => {
+    return (
+      <Box sx={{ width: '32rem', marginTop: '5rem' }}>
+        <Label>
+          <p>이메일 입력</p>
+        </Label>
+        <InputDiv>
+          <CotatoIcon icon="user-solid" color={(theme) => theme.colors.gray60} />
+          <InputBox
+            id="id"
+            name="id"
+            type="text"
+            placeholder="본인의 이메일(아이디)를 입력해주세요."
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </InputDiv>
+        {renderInputError()}
+      </Box>
+    );
+  };
+
+  return (
+    <Wrapper>
+      <CotatoPanel size="long" textImgSrc={panelText} />
+      {renderInputField()}
+      <CotatoButton width="base" height="long" color="black" text="버튼" />
+    </Wrapper>
+  );
+};
+
+//
+//
+//
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 6rem !important;
+`;
+
+const Label = styled.div`
+  width: 100%;
+  display: flex;
+  p {
+    color: ${({ theme }) => theme.colors.common.black};
+    font-size: ${({ theme }) => theme.fontSize.md};
+    margin: 0;
+  }
+`;
+
+const InputDiv = styled.div`
+  width: 100%;
+  height: 3.25rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 1.2rem 0.75rem;
+  margin: 0.5rem 0;
+  gap: 0.625rem;
+  border-radius: 0.5rem;
+  border: 2px solid ${({ theme }) => theme.colors.primary100};
+`;
+
+const InputBox = styled.input`
+  width: 100%;
+  border: none;
+  background: none;
+  outline: none;
+  font-family: Pretendard;
+`;
+
+const Error = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  gap: 0.35rem;
+  span {
+    font-size: ${({ theme }) => theme.fontSize.sm};
+    color: ${({ theme }) => theme.colors.secondary80};
+  }
+`;
+
+export default SendAuthEmail;
