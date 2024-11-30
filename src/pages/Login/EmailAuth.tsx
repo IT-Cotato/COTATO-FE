ea73fe45e8f8b5ec37ba735adab3a773d79a0356
MinJaeSon <mjsonsm@sookmyung.ac.kr>
2024-12-01 00:28:29 +0900

    feat: publish inputting auth code page

--- src/pages/Login/EmailAuth.tsx
+++ src/pages/Login/EmailAuth.tsx
@@ -1,49 +1,66 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import api from '@/api/api';
+import CotatoPanel from '@components/CotatoPanel';
+import panelText from '@assets/find_password_sending_email_panel_text.svg';
+import CotatoButton from '@components/CotatoButton';
+
+//
+//
+//
 
 interface EmailAuthProps {
   goToNextStep: () => void;
   email: string;
-  setEmail: React.Dispatch<React.SetStateAction<string>>;
 }
 
+type InputRefType = (HTMLInputElement | null)[];
+
+//
+//
+//
+
+const CODE_LENGTH = 6;
+
+//
+//
+//
+
 const EmailAuth: React.FC<EmailAuthProps> = ({ goToNextStep, email }) => {
-  const [inputs, setInputs] = useState<number[]>(Array(6).fill(null));
-  const inputRef = useRef<any>(Array(6).fill(null));
+  const [input, setInput] = useState<number[]>(Array(CODE_LENGTH).fill(null));
+  const inputRef = useRef<InputRefType>(Array(CODE_LENGTH).fill(null));
 
   /**
    *
    */
   const handlePasteEvent = (value: string) => {
-    const newInputs = [...inputs];
-    for (let i = 0; i < 6; i++) {
-      newInputs[i] = parseInt(value[i]);
+    const newInput = [...input];
+    for (let i = 0; i < CODE_LENGTH; i++) {
+      newInput[i] = parseInt(value[i]);
     }
-    setInputs(newInputs);
+    setInput(newInput);
   };
 
   /**
    *
    */
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
-    if (e.target.value.length >= 6) {
+    if (e.target.value.length >= CODE_LENGTH) {
       handlePasteEvent(e.target.value);
       return;
     }
 
-    const newInputs = [...inputs];
+    const newInput = [...input];
     let value = e.target.value;
 
-    if (newInputs[idx] !== null) {
-      const prevValue = inputs[idx].toString();
+    if (newInput[idx] !== null) {
+      const prevValue = input[idx].toString();
       value = e.target.value.replace(prevValue, '');
     }
 
-    newInputs[idx] = parseInt(value);
-    setInputs(newInputs);
+    newInput[idx] = parseInt(value);
+    setInput(newInput);
 
-    if (idx < inputs.length - 1) {
+    if (idx < input.length - 1) {
       inputRef.current[idx + 1]?.focus();
     }
   };
@@ -51,72 +68,49 @@ const EmailAuth: React.FC<EmailAuthProps> = ({ goToNextStep, email }) => {
   /**
    *
    */
-  const handleAuthCode = async () => {
-    const result = await api
-      .get('/v1/api/auth/verification', {
-        params: {
-          email: email,
-          code: inputs.join(''),
-          type: 'find-password',
-        },
-      })
-      .then((res) => {
-        alert('인증이 완료되었습니다.');
-        localStorage.setItem('tokenForUpdatePW', res.data.accessToken);
-
-        return true;
-      })
-      .catch((err) => {
-        console.log(err);
-        alert('인증번호가 일치하지 않습니다.');
-
-        inputRef.current.forEach((el: any) => {
-          el.value = '';
-        });
-
-        setInputs(Array(6).fill(null));
-
-        return false;
-      });
-
-    return result;
+  const renderGuideMessage = () => {
+    return (
+      <Message>
+        <span>{email}</span>(으)로 인증 이메일이 발송 되었습니다.
+        <br />
+        이메일에서 코드 {CODE_LENGTH}자리를 아래 박스에 입력해주세요.
+      </Message>
+    );
   };
 
   /**
    *
    */
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (inputs.some((el) => el === null)) {
-      alert('인증번호를 입력해주세요.');
-      return;
-    }
-
-    const authResult = await handleAuthCode();
-
-    if (authResult) {
-      goToNextStep();
-    }
+  const renderInputField = () => {
+    return (
+      <InputDiv>
+        {input.map((num, idx) => (
+          <InputBox
+            filled={input[idx] !== null}
+            key={idx}
+            type="number"
+            value={num}
+            ref={(el) => (inputRef.current[idx] = el)}
+            onChange={(e) => {
+              handleInputChange(e, idx);
+            }}
+          />
+        ))}
+      </InputDiv>
+    );
   };
 
   /**
    *
    */
-  const handleResendClick = async () => {
-    api
-      .post(
-        '/v1/api/auth/verification',
-        { email: email },
-        {
-          params: {
-            type: 'find-password',
-          },
-        },
-      )
-      .then(() => {
-        alert('인증 이메일이 재발송 되었습니다.');
-      });
+  const renderInfoMessage = () => {
+    return (
+      <Message>
+        인증 메일은 발송 시점부터 30분 유효하며,
+        <br />
+        재발송 시 기본 인증코드는 만료됩니다.
+      </Message>
+    );
   };
 
   //
@@ -132,130 +126,75 @@ const EmailAuth: React.FC<EmailAuthProps> = ({ goToNextStep, email }) => {
 
   return (
     <Wrapper>
-      <h3>승인 요청 이메일이 발송되었습니다.</h3>
-      <p>
-        <span>{email}</span>(으)로 인증 이메일이 발송 되었습니다.
-        <br />
-        이메일에서 코드 6자리를 아래 박스에 입력해주세요.
-      </p>
-      <Form onSubmit={onSubmit}>
-        <InputContainer>
-          {inputs.map((input, idx) => {
-            return (
-              <InputBox
-                key={idx}
-                type="number"
-                value={input}
-                onChange={(e) => {
-                  handleInputChange(e, idx);
-                }}
-                ref={(el) => (inputRef.current[idx] = el)}
-                filled={inputs[idx] !== null}
-              />
-            );
-          })}
-        </InputContainer>
-        <p>
-          인증 메일은 발송 시점부터 30분 유효하며,
-          <br />
-          재발송 시 기존 인증코드는 만료됩니다.
-        </p>
-        <Button type="submit" authorized={!inputs.some((el) => el === null)}>
-          인증 완료
-        </Button>
-      </Form>
-      <p>
-        혹시 이메일을 못받으셨나요? <span onClick={handleResendClick}>메일 다시 보내기</span>
-      </p>
+      <CotatoPanel size="long" textImgSrc={panelText} />
+      {renderGuideMessage()}
+      {renderInputField()}
+      {renderInfoMessage()}
+      <CotatoButton width="base" height="base" color="black" text="버튼" />
     </Wrapper>
   );
 };
 
-export default EmailAuth;
+//
+//
+//
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  color: ${({ theme }) => theme.colors.common.black};
-  padding: 5rem 0;
-  h3 {
-    font-size: 1.8rem;
-    margin-top: 0;
-    margin-bottom: 40px;
-  }
-  p {
-    font-size: 1rem;
-    margin: 0;
-    text-align: center;
-    span {
-      color: #477feb;
-      &:last-child {
-        &:hover {
-          text-decoration: underline;
-          cursor: pointer;
-        }
-      }
-    }
-  }
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   justify-content: center;
-  width: 500px;
-  margin-top: 40px;
-  margin-bottom: 60px;
+  padding: 6rem !important;
 `;
 
-const InputContainer = styled.div`
+const Message = styled.p`
+  font-family: 'Pretendard';
+  font-size: ${({ theme }) => theme.fontSize.md};
+  color: ${({ theme }) => theme.colors.gray80_1};
+  line-height: 1.5rem;
+  text-align: center;
+  margin-top: 1.7rem;
+  margin-bottom: 0;
+  &:nth-of-type(2) {
+    margin-bottom: 2.25rem;
+  }
+  span {
+    font-family: 'Pretendard';
+    font-weight: bold;
+  }
+`;
+
+const InputDiv = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: center;
   align-items: center;
-  margin-bottom: 40px;
+  gap: 1rem;
+  margin-top: 2.6rem;
 `;
 
 const InputBox = styled.input<{ filled: boolean }>`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 0.25rem;
+  border: 2px solid ${({ theme }) => theme.colors.common.black};
+  ${({ filled }) => filled && `border-width: 3px`};
+  background-color: ${({ theme }) => theme.colors.common.white};
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 60px;
-  height: 60px;
-  border-radius: 20px;
-  border: 1px solid #7b7b7b;
-  ${({ filled }) => filled && `border: 3px solid` + '#D2E0FB'};
-  border-radius: 16px;
-  background: #fff;
+  color: ${({ theme }) => theme.colors.gray100};
+  font-size: 2.8rem;
+  font-family: 'Pretendard';
+  font-weight: 700;
   text-align: center;
   caret-color: transparent;
-  color: #000;
-  font-family: NanumSquareRound;
-  font-size: 1.2rem;
-  font-weight: 700;
   &::-webkit-outer-spin-button,
   &::-webkit-inner-spin-button {
     -webkit-appearance: none;
   }
-  &:not(:last-child) {
-    margin-right: 20px;
+  &:focus {
+    border-width: 3px !important;
   }
 `;
 
-const Button = styled.button<{ authorized?: boolean }>`
-  width: 500px;
-  height: 52px;
-  background: ${(props) => (props.authorized ? '#85C88A' : '#D7E5CA')};
-  color: #fff;
-  font-size: 1.1rem;
-  font-weight: 400;
-  border-radius: 28px;
-  border: none;
-  font-family: NanumSquareRound;
-  margin-top: 32px;
-  &:hover {
-    cursor: pointer;
-  }
-`;
+export default EmailAuth;
