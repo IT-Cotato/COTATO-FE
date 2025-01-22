41c4abb7cdb53aadbbff28a62b863b8d416bddc4
WONYOUNG-HC <whdnjsdud551@gmail.com>
2025-01-22 23:33:37 +0900

    refactor: get icon  from const list

--- src/pages/CS/CSFirstSection/CSFirstSectionHeader.tsx
+++ src/pages/CS/CSFirstSection/CSFirstSectionHeader.tsx
@@ -3,7 +3,21 @@ import { ReactComponent as C } from '@assets/C.svg';
 import { ReactComponent as S } from '@assets/S.svg';
 import { ReactComponent as Gyo } from '@assets/gyo.svg';
 import { ReactComponent as Yook } from '@assets/yook.svg';
-import { Box, Stack, styled } from '@mui/material';
+import { Box, Stack } from '@mui/material';
+import { styled } from 'styled-components';
+
+//
+//
+//
+
+const StyledC = styled(C)`
+  width: 4.625rem;
+  height: 5rem;
+  margin-top: 1.5rem;
+  margin-right: 0.5rem;
+`;
+
+const ICON_LIST = [StyledC, S, Gyo, Yook];
 
 //
 //
@@ -17,34 +31,13 @@ const CSFirstSectionHeader = () => {
         justifyContent: 'center',
       }}
     >
-      <Stack
-        sx={{
-          marginRight: '0.5rem',
-          marginTop: '1.25rem',
-        }}
-      >
-        <StyledC />
-      </Stack>
-      <Stack>
-        <S />
-      </Stack>
-      <Stack>
-        <Gyo />
-      </Stack>
-      <Stack>
-        <Yook />
-      </Stack>
+      {ICON_LIST.map((Icon, index) => (
+        <Stack key={index}>
+          <Icon />
+        </Stack>
+      ))}
     </Box>
   );
 };
 
 export default CSFirstSectionHeader;
-
-//
-//
-//
-
-const StyledC = styled(C)`
-  width: 74px;
-  height: 80px;
-`;
