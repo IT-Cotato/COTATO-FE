40cfe9e33dd595e598b790e0dcba678a44b883fc
JO WONYOUNG <whdnjsdud551@gmail.com>
2025-06-04 17:58:02 +0900

    feat: control slide by scroll

--- src/pages/CS/CSThirdSection/CSThirdSectionSlide.tsx
+++ src/pages/CS/CSThirdSection/CSThirdSectionSlide.tsx
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import CSPPTImages from '@assets/cs_ppt';
 import { Box } from '@mui/material';
-import 'swiper/css';
 import CSThirdSectionSlideButton from './CSThirdSectionSlideButton';
 import { Swiper as SwiperClass } from 'swiper/types';
 import { useBreakpoints } from '@/hooks/useBreakpoints';
 import { media } from '@theme/media';
+import { Mousewheel } from 'swiper/modules';
+import 'swiper/css';
 
 //
 //
@@ -71,8 +72,8 @@ const CSThirdSectionSlide = ({ onChangeSlide }: CSThirdSecionSlideProps) => {
         onClick={() => handleSlideButtonClick('left')}
       />
       <StyledSwiper
-        // centeredSlides={isLandScapeOrSmaller}
-        centeredSlides
+        modules={[Mousewheel]}
+        mousewheel
         width={isLandScapeOrSmaller ? MOBILE_SLIDE_WIDTH : DESKTOP_SLIDE_WIDTH}
         spaceBetween={DESKTOP_SLIDE_WIDTH / 8}
         onSwiper={setSwiperInstance}
